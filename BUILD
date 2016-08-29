6cc980d210f7f1483a4d1f29f432875d5e56602d
Steven Dee <i@wholezero.org>
2016-08-29 21:30:37 +0000

    s/all/target/
    :all is a wildcard.

--- BUILD
+++ BUILD
@@ -1,10 +1,11 @@
 load("@bazel_tools//tools/build_defs/pkg:pkg.bzl", "pkg_tar")
 
 pkg_tar(
-    name = "all",
+    name = "target",
     files = [
         "//src:serve",
         "//src:index.html.gz",
     ],
+    package_dir = "target",
     mode = "0755",
 )
--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 # Copyright 2016 Steven Dee. All rights reserved.
 set -eu
 
-bazel build :all --compilation_mode=opt
+bazel build :target --compilation_mode=opt
 rm -rf target
-mkdir -p target
-tar xf bazel-bin/all.tar -C target
+tar xf bazel-bin/target.tar
 spk dev
